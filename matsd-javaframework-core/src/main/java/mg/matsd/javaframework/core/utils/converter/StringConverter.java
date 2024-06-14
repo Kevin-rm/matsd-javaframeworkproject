2f24c67120d327eb49749b0402d91b53e9eb120e
Kevin-rm <kevinramarozatovo@gmail.com>
2024-06-14 10:51:43 +0300

    added more functionnalities and correct the bugs

--- matsd-javaframework-core/src/main/java/mg/matsd/javaframework/core/utils/converter/StringConverter.java
+++ matsd-javaframework-core/src/main/java/mg/matsd/javaframework/core/utils/converter/StringConverter.java
@@ -3,6 +3,12 @@ package mg.matsd.javaframework.core.utils.converter;
 import mg.matsd.javaframework.core.exceptions.TypeMismatchException;
 import mg.matsd.javaframework.core.utils.Assert;
 
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Set;
 
 public final class StringConverter {
@@ -11,7 +17,7 @@ public final class StringConverter {
 
     private StringConverter() { }
 
-    public static  <T> T convert(String value, Class<T> type) throws TypeMismatchException {
+    public static <T> T convert(String value, Class<T> type) throws TypeMismatchException {
         Assert.notNull(type, "L'argument type ne peut pas être \"null\"");
         validateArgument(value);
 
@@ -28,7 +34,19 @@ public final class StringConverter {
                 result = (T) Float.valueOf(value);
             else if (type == boolean.class || type == Boolean.class)
                 result = (T) toBoolean(value);
-            else throw new UnsupportedConversionTypeException(type);
+            else if (type == LocalDate.class)
+                result = (T) LocalDate.parse(value);
+            else if (type == LocalDateTime.class)
+                result = (T) LocalDateTime.parse(value);
+            else if (type == LocalTime.class)
+                result = (T) LocalTime.parse(value);
+            else if (type == Date.class)
+                result = (T) Date.valueOf(value);
+            else if (type == Timestamp.class)
+                result = (T) Timestamp.valueOf(value);
+            else if (type == Time.class)
+                result = (T) Time.valueOf(value);
+            else throw new UnsupportedTypeConversionException(type);
         } catch (IllegalArgumentException e) {
             throw new TypeMismatchException(String.format(
                 "La chaîne de caratères \"%s\" ne peut pas être convertie en \"%s\"", value, type.getName()
--- matsd-javaframework-core/src/main/java/mg/matsd/javaframework/core/utils/converter/UnsupportedTypeConversionException.java
+++ matsd-javaframework-core/src/main/java/mg/matsd/javaframework/core/utils/converter/UnsupportedTypeConversionException.java
@@ -2,10 +2,10 @@ package mg.matsd.javaframework.core.utils.converter;
 
 import mg.matsd.javaframework.core.exceptions.BaseException;
 
-class UnsupportedConversionTypeException extends BaseException {
+class UnsupportedTypeConversionException extends BaseException {
     private static final String PREFIX = "Conversion en \"String\" non pris en charge pour le type";
 
-    UnsupportedConversionTypeException(Class<?> type) {
+    UnsupportedTypeConversionException(Class<?> type) {
         super(String.format("%s", type.getName()), PREFIX);
     }
 }
