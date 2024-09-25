d9c5c92c4d2302e6086357ee2dc7313f91315f7a
Kevin-rm <kevinramarozatovo@gmail.com>
2024-09-25 12:29:19 +0300

    deleted unuseful class

--- matsd-javaframework-orm/src/main/java/mg/matsd/javaframework/orm/proxy/LazyLoader.java
+++ matsd-javaframework-orm/src/main/java/mg/matsd/javaframework/orm/proxy/LazyLoader.java
@@ -1,5 +1,30 @@
 package mg.matsd.javaframework.orm.proxy;
 
-public class LazyLoader {
+import mg.matsd.javaframework.orm.mapping.Entity;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class LazyLoader implements InvocationHandler {
+    private final Object target;
+    private final Entity entity;
+
+    public LazyLoader(Object target, Entity entity) {
+        this.target = target;
+        this.entity = entity;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return method.invoke(proxy, args);
+    }
+
+    public static Object createProxy(Object target, Entity entity) {
+        return Proxy.newProxyInstance(
+            entity.getClazz().getClassLoader(),
+            entity.getClazz().getInterfaces(),
+            new LazyLoader(target, entity)
+        );
+    }
 }
