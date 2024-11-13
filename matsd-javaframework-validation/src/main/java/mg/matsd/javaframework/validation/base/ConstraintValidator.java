eead5456b2fd912ed7747d1a5c4ed81308667722
Kevin-rm <kevinramarozatovo@gmail.com>
2024-11-13 18:52:11 +0300

    begin with the implementation

--- matsd-javaframework-validation/src/main/java/mg/matsd/javaframework/validation/base/ConstraintValidator.java
+++ matsd-javaframework-validation/src/main/java/mg/matsd/javaframework/validation/base/ConstraintValidator.java
@@ -3,5 +3,8 @@ package mg.matsd.javaframework.validation.base;
 import java.lang.annotation.Annotation;
 
 public interface ConstraintValidator<A extends Annotation, T> {
+
+    void initialize(A constraintAnnotation);
+
     boolean isValid(T t);
 }
--- matsd-javaframework-validation/src/main/java/mg/matsd/javaframework/validation/base/Validator.java
+++ matsd-javaframework-validation/src/main/java/mg/matsd/javaframework/validation/base/Validator.java
@@ -2,7 +2,7 @@ package mg.matsd.javaframework.validation.base;
 
 public final class Validator {
 
-    public <T> void doValidate(T t, Class<?> group) {
-           
+    public <T> void doValidate(T t, Class<?>... groups) {
+
     }
 }
