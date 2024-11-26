c160fad62fa253f4a0ad4721a90dcb7f34e660a4
Kevin-rm <kevinramarozatovo@gmail.com>
2024-11-26 19:37:13 +0300

    Update ConnectorInstantiationException.java

--- matsd-javaframework-orm/src/main/java/mg/matsd/javaframework/orm/connection/ConnectorInstantiationException.java
+++ matsd-javaframework-orm/src/main/java/mg/matsd/javaframework/orm/connection/ConnectorInstantiationException.java
@@ -2,7 +2,7 @@ package mg.matsd.javaframework.orm.connection;
 
 import mg.matsd.javaframework.core.exceptions.BaseException;
 
-class ConnectorInstantiationException extends BaseException {
+public class ConnectorInstantiationException extends BaseException {
     private static final String PREFIX = "Erreur lors de l'instanciation du connecteur pour la base de données";
 
     ConnectorInstantiationException(Throwable cause) {
