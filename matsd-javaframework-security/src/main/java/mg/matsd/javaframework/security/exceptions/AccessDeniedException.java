ff84253af9f19abd10506486969ffde9761e5e42
Kevin-rm <kevinramarozatovo@gmail.com>
2025-01-12 09:05:12 +0300

    Update AccessDeniedException.java

--- matsd-javaframework-security/src/main/java/mg/matsd/javaframework/security/exceptions/AccessDeniedException.java
+++ matsd-javaframework-security/src/main/java/mg/matsd/javaframework/security/exceptions/AccessDeniedException.java
@@ -3,6 +3,8 @@ package mg.matsd.javaframework.security.exceptions;
 import mg.matsd.javaframework.core.exceptions.BaseException;
 
 public class AccessDeniedException extends BaseException {
+    private static final String PREFIX = "Accès refusé";
+
     public AccessDeniedException(String message) {
         super(message);
     }
