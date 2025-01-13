717029de07169fbae77084641bbea58a4a4ed29e
Kevin-rm <kevinramarozatovo@gmail.com>
2025-01-13 20:08:49 +0300

    added @Documented on the annotations in core

--- matsd-javaframework-core/src/main/java/mg/matsd/javaframework/core/annotations/Inject.java
+++ matsd-javaframework-core/src/main/java/mg/matsd/javaframework/core/annotations/Inject.java
@@ -3,8 +3,6 @@ package mg.matsd.javaframework.core.annotations;
 import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.PARAMETER)
+@Target({ElementType.FIELD, ElementType.CONSTRUCTOR})
 @Documented
-public @interface Identifier {
-    String value();
-}
+public @interface Inject { }
--- matsd-javaframework-core/src/main/java/mg/matsd/javaframework/core/annotations/Scope.java
+++ matsd-javaframework-core/src/main/java/mg/matsd/javaframework/core/annotations/Scope.java
@@ -1,12 +1,10 @@
 package mg.matsd.javaframework.core.annotations;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
+@Documented
 public @interface Scope {
     String value() default "singleton";
 }
