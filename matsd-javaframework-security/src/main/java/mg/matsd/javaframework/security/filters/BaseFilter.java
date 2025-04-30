2ef78fb5a2260a7cfd4daecdddf013a3ee1a0039
Kevin-rm <kevinramarozatovo@gmail.com>
2025-04-30 20:57:37 +0300

    Create BaseFilter.java

---
+++ matsd-javaframework-security/src/main/java/mg/matsd/javaframework/security/filters/BaseFilter.java
@@ -0,0 +1,60 @@
+package mg.matsd.javaframework.security.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import mg.matsd.javaframework.core.annotations.Nullable;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+public abstract class BaseFilter implements Filter {
+    protected static final Logger LOGGER = LogManager.getLogger(BaseFilter.class);
+    private FilterConfig filterConfig;
+
+    public FilterConfig getFilterConfig() {
+        return filterConfig;
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+        LOGGER.debug("Initialisation du filtre: \"{}\"", filterConfig.getFilterName());
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        if (!(servletRequest  instanceof HttpServletRequest httpServletRequest)   ||
+            !(servletResponse instanceof HttpServletResponse httpServletResponse) ||
+            shouldIgnore(httpServletRequest)
+        ) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        FilterChainDecision decision = preHandle(httpServletRequest, httpServletResponse);
+        switch (decision) {
+            case CONTINUE -> {
+                filterChain.doFilter(servletRequest, servletResponse);
+                postHandle(httpServletRequest, httpServletResponse);
+            }
+            case SKIP_CHAIN -> postHandle(httpServletRequest, httpServletResponse);
+            case STOP -> { }
+            default -> throw new IllegalStateException("Valeur de FilterChainDecision non reconnue: " + decision);
+        }
+    }
+
+    @Nullable
+    public abstract FilterChainDecision preHandle(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException;
+
+    public void postHandle(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException { }
+
+    public boolean shouldIgnore(HttpServletRequest request) {
+        return false;
+    }
+
+    public enum FilterChainDecision { CONTINUE, SKIP_CHAIN, STOP }
+}
