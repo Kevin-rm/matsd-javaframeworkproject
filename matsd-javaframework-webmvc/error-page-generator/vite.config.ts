9f66d4a379bab874db9173f9ae52e9170b5765d5
Kevin-rm <kevinramarozatovo@gmail.com>
2025-05-17 07:22:41 +0300

    improve vite config for build

--- matsd-javaframework-webmvc/error-page-generator/vite.config.ts
+++ matsd-javaframework-webmvc/error-page-generator/vite.config.ts
@@ -2,13 +2,31 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import * as path from "node:path";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    }
+  },
+  build: {
+    emptyOutDir: true,
+    assetsInlineLimit: 100000000,
+    cssCodeSplit: false,
+    minify: process.env.NODE_ENV === "production" ? "terser" : "esbuild",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ["console.info", "console.debug", "console.warn"]
+      },
+      format: { comments: false }
+    },
+    rollupOptions: {
+      output: { inlineDynamicImports: true }
     }
   }
 })
